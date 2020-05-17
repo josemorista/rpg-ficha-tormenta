087af036fc2810bd3ed5a87434c5bebc1970e57e
josemorista <josemorista@id.uff.br>
2020-05-17 20:52:12 -0300

    Updates

---
+++ precache-manifest.17b022eab5c2503e8e8e3cf9cea2eb28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "783a472741359ef316b23892601485a9",
+    "url": "/rpg-ficha-tormenta/index.html"
+  },
+  {
+    "revision": "f92db467997c6c0bd9f6",
+    "url": "/rpg-ficha-tormenta/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "96477f17196a3485dd29",
+    "url": "/rpg-ficha-tormenta/static/js/2.9c916b6a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/rpg-ficha-tormenta/static/js/2.9c916b6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f92db467997c6c0bd9f6",
+    "url": "/rpg-ficha-tormenta/static/js/main.e3c1ba57.chunk.js"
+  },
+  {
+    "revision": "8bfd643824022c0bc688",
+    "url": "/rpg-ficha-tormenta/static/js/runtime-main.fb4b4e89.js"
+  },
+  {
+    "revision": "a153ac94b3d6e36530d42ac06e6b8bfe",
+    "url": "/rpg-ficha-tormenta/static/media/bg.a153ac94.jpg"
+  }
+]);
